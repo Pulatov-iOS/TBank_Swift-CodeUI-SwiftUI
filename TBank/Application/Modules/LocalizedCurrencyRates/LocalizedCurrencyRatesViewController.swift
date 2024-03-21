392e9f1c36d73e0f1eaf6ba8b0d09d7b340e447c
Daniil <idanik.mega@gmail.com>
2024-03-21 23:32:25 +0300

    add button and screen

--- TBank/Application/Modules/LocalizedCurrencyRates/LocalizedCurrencyRatesViewController.swift
+++ TBank/Application/Modules/LocalizedCurrencyRates/LocalizedCurrencyRatesViewController.swift
@@ -6,10 +6,13 @@ final class LocalizedCurrencyRatesViewController: UIViewController {
     // MARK: - Public Properties
     var viewModel: LocalizedCurrencyRatesViewModel!
     
+    private let titleLabel = UILabel()
+    private let setupButton = UIButton()
+    
     // MARK: - LyfeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
         addSubviews()
         configureConstraints()
         configureUI()
@@ -18,15 +21,34 @@ final class LocalizedCurrencyRatesViewController: UIViewController {
     
     // MARK: - Methods
     private func addSubviews() {
-        
+        view.addSubview(titleLabel)
+        view.addSubview(setupButton)
     }
     
     private func configureConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
+        setupButton.translatesAutoresizingMaskIntoConstraints = false
+        setupButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor).isActive = true
+        setupButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -26).isActive = true
     }
     
     private func configureUI() {
-        navigationItem.title = NSLocalizedString("App.LocalizedCurrencyRates.NavigationItemTitle", comment: "")
+        titleLabel.text = NSLocalizedString("App.LocalizedCurrencyRates.NavigationItemTitle", comment: "")
+        titleLabel.textColor = .black
+        titleLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        
+        setupButton.tintColor = .black
+        setupButton.setImage(UIImage(systemName: "gearshape"), for: .normal)
+        let symbolConfigurationAdd = UIImage.SymbolConfiguration(pointSize: 25)
+        setupButton.setPreferredSymbolConfiguration(symbolConfigurationAdd, forImageIn: .normal)
+        setupButton.addTarget(self, action: #selector(tapOnSetupButton), for: .touchUpInside)
+    }
+    
+    @objc func tapOnSetupButton() {
+        print("update")
     }
     
     private func bindings() {
