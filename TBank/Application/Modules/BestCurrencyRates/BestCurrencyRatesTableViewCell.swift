33c4115b3cbd67cb231e50399480f94cf84c8cc7
Daniil <idanik.mega@gmail.com>
2024-03-21 13:14:26 +0300

    fix UI

--- TBank/Application/Modules/BestCurrencyRates/BestCurrencyRatesTableViewCell.swift
+++ TBank/Application/Modules/BestCurrencyRates/BestCurrencyRatesTableViewCell.swift
@@ -26,22 +26,22 @@ final class BestCurrencyRatesTableViewCell: UITableViewCell {
         contentView.addSubview(customView)
         customView.addSubview(nameLabel)
         customView.addSubview(priceLabel)
-        customView.addSubview(imageFlag)
+        //customView.addSubview(imageFlag)
         
         customView.translatesAutoresizingMaskIntoConstraints = false
         customView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
-        customView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 27).isActive = true
-        customView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -27).isActive = true
-        customView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15).isActive = true
+        customView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        customView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
+        customView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
         
         
-        imageFlag.translatesAutoresizingMaskIntoConstraints = false
-        imageFlag.centerYAnchor.constraint(equalTo: customView.centerYAnchor).isActive = true
-        imageFlag.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 25).isActive = true
+        //imageFlag.translatesAutoresizingMaskIntoConstraints = false
+        //imageFlag.centerYAnchor.constraint(equalTo: customView.centerYAnchor).isActive = true
+        //imageFlag.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 25).isActive = true
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.centerYAnchor.constraint(equalTo: customView.centerYAnchor).isActive = true
-        nameLabel.leadingAnchor.constraint(equalTo: imageFlag.trailingAnchor, constant: 17).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: customView.leadingAnchor, constant: 17).isActive = true
         
         priceLabel.translatesAutoresizingMaskIntoConstraints = false
         priceLabel.centerYAnchor.constraint(equalTo: customView.centerYAnchor).isActive = true
@@ -49,21 +49,17 @@ final class BestCurrencyRatesTableViewCell: UITableViewCell {
         
         contentView.layer.masksToBounds = true
         contentView.backgroundColor = .white
-        
-        customView.layer.shadowColor = UIColor.black.cgColor
-        customView.layer.shadowOffset = CGSize(width: 0, height: 5)
-        customView.layer.shadowRadius = 6
-        customView.layer.shadowOpacity = 0.20
+    
         customView.layer.borderWidth = 2.0
         customView.layer.borderColor = UIColor.black.cgColor
         customView.layer.cornerRadius = 5
         customView.backgroundColor = .white
         
         nameLabel.textColor = .black
-        nameLabel.font = UIFont(name: "Rubik-Regular", size: 25)
+        nameLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
         
         priceLabel.textColor = .black
-        priceLabel.font = UIFont(name: "Rubik-Regular", size: 25)
+        priceLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
     }
     
     // MARK: - Configuration
