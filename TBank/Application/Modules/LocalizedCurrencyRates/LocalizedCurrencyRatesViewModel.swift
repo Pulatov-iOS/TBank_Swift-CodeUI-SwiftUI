8a4d9fb05f5c47a6aa73ba1389562c43e6a34948
Daniil <idanik.mega@gmail.com>
2024-03-22 10:54:05 +0300

    fix problems

--- TBank/Application/Modules/LocalizedCurrencyRates/LocalizedCurrencyRatesViewModel.swift
+++ TBank/Application/Modules/LocalizedCurrencyRates/LocalizedCurrencyRatesViewModel.swift
@@ -1,10 +1,22 @@
-import Combine
+import Foundation
 
 final class LocalizedCurrencyRatesViewModel {
+    private var networkManager: NetworkManagerCurrency
     
-    init() {
-        
+    var currencyRates: [CurrencyRate] = []
+    
+    init(networkManager: NetworkManagerCurrency) {
+        self.networkManager = networkManager
     }
     
+    func fetchExchangeRates(completion: @escaping (Error?) -> Void) {
+        networkManager.fetchExchangeRates { [weak self] ratesOrNil in
+            guard let rates = ratesOrNil else {
+                completion(NSError(domain: "NetworkError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to fetch exchange rates"]))
+                return
+            }
+            self?.currencyRates = rates
+            completion(nil)
+        }
+    }
 }
-
